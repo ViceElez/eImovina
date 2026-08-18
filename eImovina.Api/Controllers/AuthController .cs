bd94d307623faf1987a2a4f762980a76d1f089a1
ViceElez <vice.elezovic12@gmail.com>
2026-08-18 14:24:54 +0200

    add login

---
+++ eImovina.Api/Controllers/AuthController .cs
@@ -0,0 +1,30 @@
+﻿using eImovina.Api.Data;
+using eImovina.Api.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using eImovina.Shared.DTOs.Auth;
+
+namespace eImovina.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController: ControllerBase
+    {
+        private readonly eImovinaDbContext _context;
+        private readonly JwtTokenService _tokenService;
+
+        public AuthController(eImovinaDbContext context, JwtTokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            // Implementation for login logic
+            return Ok();
+        }
+    }
+}
